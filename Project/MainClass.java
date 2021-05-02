4a7500828726657f8cfa3f4ac53b73af04bdbd3e
srknk <srknk@DESKTOP-6N17FDL.local>
2021-05-03 00:47:21 +0300

    son

--- Project/MainClass.java
+++ Project/MainClass.java
@@ -14,6 +14,7 @@ public class MainClass extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		System.out.println("oldu sonunda ");
+		System.out.println("evet oldu");
 		BorderPane mainPane = new BorderPane();
 		
 		Pane topPane = new Pane();
