7d6a3546bb886e52dcd46363f5129643ed520cb0
ömer <zalman@DESKTOP-VPJV837.home>
2021-05-20 12:15:45 +0300

    comment game profile

--- Project/GameProfile.java
+++ Project/GameProfile.java
@@ -3,23 +3,26 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+// The purpose of this class is to create, save and load the profile for the game. 
 public class GameProfile {
 	
-	boolean isStarted;
-	
 	public GameProfile() {
+		//When creating game profile object checks if there is a saved game if not it creates a new profile using createProfile() method.
 		if(!new File("profile/save.txt").exists() || !new File("profile/highscores.txt").exists() ) {
 			createProfile();
 		}	
 	}
+	
+	//It saves the current status of game to the folder profile inside the project file.
 	public void saveProfile(int currentLevel,String hitLabel,String scoreLabel, Box[][] boxes,double volume){
+		//Checks the profile folder if there is a missing file.
 		if(!new File("profile/save.txt").exists() || !new File("profile/highscores.txt").exists() ) {
 			createProfile();
 		}
 		File file = new File("profile/");
 		File saveFile = new File(file+"/save.txt");
 		File saveBox = new File(file+"/boxes.txt");
+		//Write data to text files.
 		try {
 			PrintWriter writer = new PrintWriter(saveFile);
 			writer.write(String.format("%s/%s/%s/%s","Level "+currentLevel,hitLabel,scoreLabel,volume));
@@ -36,7 +39,7 @@ public class GameProfile {
 			e.printStackTrace();
 		}	
 	}
-	
+	//It saves player's new high score to the profile/highscores.txt file.
 	public void saveNewHighScore(int currentLevel,int score,int levelCount) throws Exception {
 		String path = "profile/highscores.txt";
 		Scanner scoresFile = new Scanner(new File(path));
@@ -61,7 +64,7 @@ public class GameProfile {
 			writer.close();
 		}
 	}
-	
+	//It loads player's high score for the given level as parameter.
 	public String getHighScore(int level) throws Exception {
 		String path = "profile/highscores.txt";
 		String highScore = "*";
@@ -75,7 +78,7 @@ public class GameProfile {
 		scoresFile.close();
 		return highScore;
 	}
-	
+	//It creates new profile with the initial values to the profile folder in project.
 	public void createProfile() {
 		File file = new File("profile");
 		file.mkdir();
@@ -92,7 +95,7 @@ public class GameProfile {
 			e.printStackTrace();
 		}	
 	}
-
+	//It loads saved labels data to the GamePane class. 
 	public String[] loadLabels() {
 		try {
 			Scanner reader = new Scanner(new File("profile/save.txt"));
@@ -104,7 +107,7 @@ public class GameProfile {
 			return null;
 		}		
 	}
-	
+	//It loads all the high scores to the MenuPane class so that player can see her/his high scores from the menu.
 	public static ArrayList<String> getHighScores(){
 		File highScoresFile = new File("profile/highscores.txt");
 		if(!highScoresFile.exists())
@@ -123,7 +126,7 @@ public class GameProfile {
 			return null;
 		}
 	}
-	
+	//It loads volume value to the MenuPane.
 	public static double getVolume() {
 		File file = new File("profile/save.txt");
 		if(!file.exists()) {
